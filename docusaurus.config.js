4432143eeb360fbf02dcefef61889588e4ac1515
Alexis Delage <alexidelage@gmail.com>
2022-10-03 11:06:29 +0200

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Les ICAres',
   tagline: 'Compétition Inter-Centrale des Arts',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://www.icares-centrale.fr',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
